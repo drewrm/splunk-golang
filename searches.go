3535631bd67969271f224dbf2503e4d54e278c7f
efernandezbrei <eferandezbrei@hotels.com>
2019-09-08 20:32:28 +0100

    search feature added with earliest and latest

--- searches.go
+++ searches.go
@@ -7,9 +7,11 @@ import (
 
 func (conn SplunkConnection) SearchSync(searchString string) (string, error) {
 	data := make(url.Values)
-	data.Add("search",searchString)
+	data.Add("search", searchString)
 	data.Add("max_count", "1")
-	data.Add("max_time", "1")
+	data.Add("max_time", "60")
+	data.Add("earliest_time", "-10m")
+	data.Add("latest_time", "now")
 	data.Add("output_mode", "json")
 
 	response, err := conn.httpPost(fmt.Sprintf("%s/services/search/jobs/export", conn.BaseURL), &data)
--- searches_test.go
+++ searches_test.go
@@ -1,26 +1,19 @@
 package splunk
 
 import (
-	"github.com/prometheus/common/log"
-	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
 	"testing"
 )
 
 func TestICouldSearchSync(t *testing.T) {
 
-	connection, err := CreateConnectionFromEnvironment()
+	connection, _ := CreateConnectionFromEnvironment()
+	require.NotNil(t, connection, "error on searching")
 
-	assert.NotNil(t, connection, "error on searching")
+	sessionKey, _ := connection.Login()
+	require.NotNil(t, sessionKey, "error on searching")
 
-	sessionKey, err := connection.Login()
-
-	assert.NotNil(t, sessionKey, "error on searching")
-
-	searchString := "search index=hotels_com kubernetes.pod_id=60500b25-cbf3-11e9-8768-02b2860aca1c"
-	searchResponse, err := connection.SearchSync(searchString)
-
-	assert.Nil(t, err, "error on searching")
-	log.Infof("search is %s ", searchResponse)
-	assert.NotNil(t, searchResponse, "error on searching")
-
+	searchString := "search index=_internal linecount=1"
+	searchResponse, _ := connection.SearchSync(searchString)
+	require.NotNil(t, searchResponse, "error on searching")
 }
